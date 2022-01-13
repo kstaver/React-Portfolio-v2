840be6d044bf8060513049aab53edb538eecdf18
kstaver <staverkendra@gmail.com>
2022-01-12 18:27:36 -0800

    choosing icons is hard

--- src/components/Body/Skills/index.js
+++ src/components/Body/Skills/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Separator from '../../commons/separator';
+import { SkillsData } from '../../data/skills';
+import SkillCard from './skill-card';
+import './skills.css';
 
 function Skills() {
+    const data = SkillsData;
     return (
         <div>
             <Separator />
             <label className='section-title'>Skills</label>
-            <div>
-                
+            <div className='skills-container'>
+                {data.map((item)=>{
+                    return(
+                        <div className='skills-section'>
+                            <label className='skills-section-title'>{item.type}</label>
+                            <div className='skills-list'>
+                                {item.list.map((skill)=>{
+                                    return(
+                                        <SkillCard skill={skill} />
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
--- src/components/data/skills.js
+++ src/components/data/skills.js
@@ -1,12 +1,48 @@
-export const SkillsData={
+export const SkillsData=[
     {
         type: "Frontend",
-        list:[
+        list: [
             {
                 name: "ReactJS",
-                icon:
+                icon: "/images/react-64.png",
+            },
+            {
+                name: "JavaScript",
+                icon: "/images/javascript-64.png",
+            },
+            {
+                name:"HTML",
+                icon: "/images/html-64.png"
+            },
+            {
+                name: "CSS",
+                icon: "/images/css-64.png"
+            },
+        ],
+    },
+    {
+        type: "Backend",
+        list: [
+            {
+                name: "MySQL",
+                icon: "/images/mysql-64.png",
+            },
+            {
+                name: "Node.js",
+                icon: "/images/node-64.png",
+            },
+            {
+                name: "Express.js",
+                icon: "/images/express-64.png",
+            },
+            {
+                name: "C++",
+                icon: "/images/c++-64.png",
+            },
+            {
+                name: "Java",
+                icon: "/images/java-64.png",
             },
         ]
-    
     }
-}
+];
