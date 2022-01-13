66ed020f0e26f16c06d3c92cc152d0897c3f4d1c
kstaver <staverkendra@gmail.com>
2022-01-13 04:05:58 -0800

    that did not work

--- src/components/Body/Skills/skill-card.js
+++ src/components/Body/Skills/skill-card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './skill-card.css';
 
-function SkillCard({ skill }) {
+function SkillCard({skill}) {
     return (
         <div className='skill-card'>
             <div className='skill-icon'>{skill.image}</div>
