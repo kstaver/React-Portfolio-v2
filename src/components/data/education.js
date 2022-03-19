4b80830e9bd4c578cbf3b748db2382282fc2d7aa
kstaver <staverkendra@gmail.com>
2022-03-19 01:02:59 -0700

    updated education section

--- src/components/data/education.js
+++ src/components/data/education.js
@@ -2,28 +2,28 @@ export const EducationData=[
     {
         school: "University of California, Berkeley Extension",
         graduation: "1/17/2022",
-        degree: "Full-Stack Web Developer Certifcation",
+        degree: "Certificate in Full Stack Web Development",
         schoolLogo: "/images/ucbe-logo.jpg",
         courses: "MERN, JavaScript, CSS, HTML, Git, APIs, MySQL, NoSQL, ORM, MVC, and PWA"
     },
     {
         school: "De Anza Community College",
         graduation: "12/11/2020",
-        degree: "Associate in Science for Computer Science",
+        degree: "Associate of Science in Computer Science",
         schoolLogo: "/images/de-anza.png",
         courses:"C++, Python, Java, Data Structures and Abstractions, and Assembly Language"
     },
     {
         school: "De Anza Community College",
         graduation: "3/25/2016",
-        degree: "Associates in Arts for Enterprise Security Professional",
+        degree: "Associates of Arts in Enterprise Security Professional",
         schoolLogo: "/images/de-anza.png",
-        courses: "Ethical Hacking, Network Security, Windows 7 Administration, Internet Concepts and TCP/IP Protocols, and Data Communication and Networking"
+        courses: "Ethical Hacking, Network Security, Windows 7 Administration, Internet Concepts and TCP/IP Protocols, and Data Communication"
     },
     {
         school: "University of California, Santa Cruz",
         graduation: "12/12/2013",
-        degree: "Bachelors of Arts for Mathematics (Pure), Minor in S.T.E.M. Education",
+        degree: "Bachelors of Arts in Mathematics (Pure), Minor in S.T.E.M. Education",
         schoolLogo: "/images/ucsc-logo.jpg",
         courses: "Linear Algebra, Discrete Mathematics, Abstract Algebra, Statistics, Calculus, Vector Calculus, Euclidean & Non-Euclidean Geometry, Number Theory, and Complex & Real Analysis"
     }
