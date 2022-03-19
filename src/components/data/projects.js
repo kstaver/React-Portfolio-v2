85222def448003e877c8fa54a45e38d5d5f11548
kstaver <staverkendra@gmail.com>
2022-03-19 01:03:10 -0700

    changing the project data

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,21 +1,24 @@
 export const ProjectData=[
     {
         id: 1,
-        title: "Culinary Eats",
-        about: "CulinaryEats is a forum based website that enourages cooks from all cultures to share their favorite recipes, without all of the mundane chatter-mag and foolery.",
-        tags: ["React", "Node", "Heroku", "CSS", "JavaScript", "HTML", "MYSQL"],
-        demo: "https://culinaryeats.herokuapp.com/",
-        github: "https://github.com/kstaver/CulinaryEats",
-        image: "/images/culinary.png"
-    },
-    {
-        id: 2,
         title: "Brew Finder",
         about: "Brew Finder is an application that allows you to search for a brewery by city. A short description and a location finder provides you with additional information about the brewery.",
         tags: ["CSS", "JavaScript", "HTML"],
         demo: "https://asecord92.github.io/brew-finder/",
         github: "https://github.com/asecord92/brew-finder",
         image: "/images/brewfinder.png"
+    },
+    {
+        id: 1,
+        title: "Book of Faces",
+        about: "An example of a back end for a social networking API. This API stores users, posts, comments, friends, and reactions. The stored data can then be edited or deleted.",
+        tags: ["React", "MongoDB", "CSS3", "JavaScript", "HTML5"],
+        demo: "https://youtu.be/N2pCil_Sdf4",
+        github: "https://github.com/kstaver/Book-Of-Faces",
+        image: "/images/culinary.png"
+    },
+    {
+
     },
     {
         id: 3,
